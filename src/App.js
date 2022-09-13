4c9502659d4fc9757c705a0f6e7677237900dc8c
trevorwavelet@gmail.com <trevorwavelet@gmail.com>
2022-09-13 17:20:12 -0400

    Created the form that the user will use to create a new post - an input for their name and and input for the body of their post + put placeholder text inside the input

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
+
 import React, { useState } from 'react';
+import CreatePostForm from './CreatePost/CreatePost';
 
-
+  
 
 function App() {
+
+
+  const [posts, setPosts] = useState([])
+  //we pass in {posts} when instantiating the post, create posts, display posts
+  
+  function addNewPost(post){
+    
+    let tempPosts = [post, ...posts]
+
+    setPosts(tempPosts);
+  }
+
   return (
     <div className="App">
-     
+     <CreatePostForm addNewPostProperty = {addNewPost}/>
     </div>
   );
 }
---
+++ src/CreatePost/CreatePost.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+
+
+const CreatePostForm = (props) => {
+    
+    const [name, setName] = useState('')
+    const [body, setBody] = useState('')
+    
+    function handleSubmit(event){
+        event.preventDefault();
+        let newPost = {
+            name: name,
+            body: body
+        }
+        console.log(newPost)
+        props.addNewPostProperty(newPost)
+    }
+    
+    return (
+        <form onSubmit={handleSubmit} className='form-grid'>
+            <div className='form-group'>
+                <label >Name</label>
+                <input placeholder='Enter your name:' type='string' className='form-control' value = {name} onChange={(event) => setName(event.target.value)}/>
+            </div>
+            
+            <div className='form-group'>
+                <label >Date</label>
+                <input placeholder='Type your new post here!' type='string' className='form-control' value ={body} onChange ={(event) => setBody(event.target.value)}/>
+            </div>
+            
+            <button type = 'submit' className='btn btn-primary' style={{'margin-top': '1em'}}>Create</button>
+        </form>
+    );
+}
+ 
+export default CreatePostForm;
